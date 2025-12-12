ab164b683d9afdcc93a4221981c2df69df603ce9
Samuel Olusanya <95101113+Jester8@users.noreply.github.com>
2025-12-12 15:21:48 +0100

    update

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Notes route working!");
+  res.send("Notes route workingss!");
 });
 
 module.exports = router;
